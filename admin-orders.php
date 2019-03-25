fc716cbe6a8e5bc905e95a6599e7bcbc8bb83bc1
Allan Camargo <allan.cdev@gmail.com>
2019-03-25 02:22:36 -0300

    Find and pagination orders

--- admin-orders.php
+++ admin-orders.php
@@ -7,6 +7,7 @@
 
     $app -> get("/admin/orders/:idorder/status", function($idorder) {
         User::verifyLogin();
+
         $order = new Order();
         $order -> get((int) $idorder);
         $page = new PageAdmin();
@@ -64,9 +65,34 @@
     $app -> get("/admin/orders", function() {
         User::verifyLogin();
 
+
+        $search = (isset($_GET['search'])) ? $_GET['search'] : "";
+        $page = (isset($_GET["page"])) ? (int) $_GET["page"] : 1;
+        if($search != "") {
+            $pagination = Order::getPageSearch($search, $page);
+        } else {
+            $pagination = Order::getPage($page);
+        }
+
+        $pages = [];
+
+        for($x = 0; $x < $pagination['pages']; $x++) {
+            array_push($pages, [
+                'href' => "/pj_final_hcode/admin/orders?" . http_build_query([
+                    "page" => $x + 1,
+                    "search" => $search
+                ]),
+                "text" => $x + 1
+            ]);
+        }
+
+
+
         $page = new PageAdmin();
         $page -> setTpl("orders", [
-            "orders" => Order::listAll()
+            "orders" => $pagination['data'],
+            "search" => $search,
+            "pages" => $pages
         ]);
 
     });
